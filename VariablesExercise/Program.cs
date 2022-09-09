ad59b8c5e28ffb0f9681df68208248a8ab1bc7e2
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-09 10:11:08 -0500

    initial commit

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string catName = "Nala";
+            int catAge = 7;
+            char Excited = '!';
+            bool isCat = true;
+            double Weight = 7.8;
+            decimal numberOfToys = 22.8m;
+
+            Console.WriteLine($"My cat's name is {catName}. She is {catAge} years old and weighs {Weight} pounds.  " +
+                $"She has {numberOfToys} toys as some are broken{Excited}  True or False Do you like cats? {isCat}");
+
+        }
+    }
+}
